a3e576810d39f487da262af1e672f2b511ac0f69
alexandregama <alexandre.gama.lima@gmail.com>
2016-02-08 16:06:22 -0200

    Initial structure

---
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -0,0 +1,13 @@
+package br.com.caelum.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class RotaPedidos {
+
+	public static void main(String[] args) throws Exception {
+
+
+	}	
+}
